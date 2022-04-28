a0b7d3cea54caa38594b988f680157d15f1693f7
TheyMiss <kutkeviciusd@gmail.com>
2022-04-29 01:09:18 +0300

    commit

--- src/common/guards/role.guard.ts
+++ src/common/guards/role.guard.ts
@@ -1,10 +1,4 @@
-import { verifySession } from 'supertokens-node/recipe/session/framework/express';
-import {
-  CanActivate,
-  ExecutionContext,
-  Injectable,
-  Session,
-} from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import Sess from 'supertokens-node/recipe/session';
 
@@ -12,30 +6,15 @@ import Sess from 'supertokens-node/recipe/session';
 export class RoleGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const roles = this.reflector.getAllAndOverride<string[]>('roles', [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-    const request = context.switchToHttp().getRequest();
-    const response = context.switchToHttp().getResponse();
-    // const session = await verifySession()(request, response, (res) => {
-    //   console.log(res);
-    // });
-    Sess.getSessionInformation();
-    console.log({ roles: request });
+    const ctx = context.switchToHttp();
+    const resp = ctx.getResponse();
 
-    if (!roles || roles.length === 0) {
-      return true;
-    }
+    const sess = await Sess.getSession(ctx.getRequest(), resp, {
+      sessionRequired: true,
+    });
+    sess.getAccessTokenPayload();
 
-    // let err = undefined;
-    // const resp = ctx.getResponse();
-    // // You can create an optional version of this by passing {sessionRequired: false} to verifySession
-    // const test = await verifySession()(ctx.getRequest(), resp, (res) => {
-    //   console.log(res);
-    //   err = res;
-    // });
-
-    return false;
+    console.log({ sess: sess.getUserId() });
+    return true;
   }
 }
